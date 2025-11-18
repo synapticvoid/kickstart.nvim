409b8116b17e5a91306f9791ec725d336a396d1b
Robin Penea <robin.penea@gmail.com>
2025-11-18 18:01:02 +0100

    Add mj and mk for slides navigation

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -19,6 +19,53 @@ vim.opt_local.commentstring = '<!--%s-->'
 vim.keymap.set('n', 'j', 'gj', { buffer = true, desc = 'Move down visual line' })
 vim.keymap.set('n', 'k', 'gk', { buffer = true, desc = 'Move up visual line' })
 
+-- Shortcut: <Leader>mc (Markdown Code block)
+-- Goal: Place cursor at: ```<cursor> with closing ``` below
+vim.keymap.set('n', '<Leader>mc', function()
+  local code_template = { '```', '```' }
+  vim.api.nvim_put(code_template, 'l', true, true)
+  -- Move up one line and append at the end (after the opening ```)
+  vim.cmd 'normal! kA'
+end, { buffer = true, silent = true, desc = 'Create code block' })
+
+-- Shortcut: <Leader>mtb (Markdown Table)
+-- Goal: Insert basic table structure
+vim.keymap.set('n', '<Leader>mtb', function()
+  local table_template = {
+    '| Column 1 | Column 2 | Column 3 |',
+    '|----------|----------|----------|',
+    '|          |          |          |',
+  }
+  vim.api.nvim_put(table_template, 'l', true, true)
+  -- Move to first cell
+  vim.cmd 'normal! 2k$'
+end, { buffer = true, silent = true, desc = 'Create markdown table' })
+
+-- Shortcut: <Leader>mi (Markdown Image)
+-- Goal: Insert image syntax ![alt](path)
+vim.keymap.set('n', '<Leader>mi', function()
+  local image_template = { '![alt text](image-path)' }
+  vim.api.nvim_put(image_template, 'l', true, true)
+  -- Position cursor at 'alt text'
+  vim.cmd 'normal! k$F[la'
+end, { buffer = true, silent = true, desc = 'Insert image' })
+
+-- INFO: Sli.dev specific shortcuts
+
+-- Shortcut: <Leader>mj (Markdown Jump to next slide)
+-- Goal: Jump to next slide separator with wraparound
+vim.keymap.set('n', '<Leader>mj', function()
+  vim.fn.setreg('/', '^---$')
+  vim.fn.search('^---$', 'w')
+end, { buffer = true, silent = true, desc = 'Jump to next slide' })
+
+-- Shortcut: <Leader>mk (Markdown Jump to previous slide)
+-- Goal: Jump to previous slide separator with wraparound
+vim.keymap.set('n', '<Leader>mk', function()
+  vim.fn.setreg('/', '^---$')
+  vim.fn.search('^---$', 'bw')
+end, { buffer = true, silent = true, desc = 'Jump to previous slide' })
+
 -- Shortcut: <Leader>mn (Markdown New Slide)
 -- Goal: Place cursor at: # <cursor> (in Insert Mode)
 vim.keymap.set('n', '<Leader>mn', function()
@@ -49,43 +96,3 @@ vim.keymap.set('n', '<Leader>mt', function()
   vim.api.nvim_put(tp_template, 'l', true, true)
   vim.cmd 'startinsert'
 end, { buffer = true, silent = true, desc = 'Create new TP-layout slide' })
-
--- Shortcut: <Leader>mc (Markdown Code block)
--- Goal: Place cursor at: ```<cursor> with closing ``` below
-vim.keymap.set('n', '<Leader>mc', function()
-  local code_template = { '```', '```' }
-  vim.api.nvim_put(code_template, 'l', true, true)
-  -- Move up one line and append at the end (after the opening ```)
-  vim.cmd 'normal! kA'
-end, { buffer = true, silent = true, desc = 'Create code block' })
-
--- Heading navigation (AZERTY-friendly)
-vim.keymap.set('n', '<Leader>mh', function()
-  vim.fn.search('^#\\+', 'W')
-end, { buffer = true, silent = true, desc = 'Next heading' })
-
-vim.keymap.set('n', '<Leader>mH', function()
-  vim.fn.search('^#\\+', 'bW')
-end, { buffer = true, silent = true, desc = 'Previous heading' })
-
--- Shortcut: <Leader>mtb (Markdown Table)
--- Goal: Insert basic table structure
-vim.keymap.set('n', '<Leader>mtb', function()
-  local table_template = {
-    '| Column 1 | Column 2 | Column 3 |',
-    '|----------|----------|----------|',
-    '|          |          |          |',
-  }
-  vim.api.nvim_put(table_template, 'l', true, true)
-  -- Move to first cell
-  vim.cmd 'normal! 2k$'
-end, { buffer = true, silent = true, desc = 'Create markdown table' })
-
--- Shortcut: <Leader>mi (Markdown Image)
--- Goal: Insert image syntax ![alt](path)
-vim.keymap.set('n', '<Leader>mi', function()
-  local image_template = { '![alt text](image-path)' }
-  vim.api.nvim_put(image_template, 'l', true, true)
-  -- Position cursor at 'alt text'
-  vim.cmd 'normal! k$F[la'
-end, { buffer = true, silent = true, desc = 'Insert image' })
