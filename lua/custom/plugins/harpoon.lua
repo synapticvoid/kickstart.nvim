bcab95140af15621a618b6b204a295d3f94ae7d1
Robin Penea <robin.penea@gmail.com>
2026-02-21 19:27:23 +0100

    Add harpoon plugin

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,31 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+  config = function()
+    local harpoon = require 'harpoon'
+    harpoon:setup()
+
+    local conf = require('telescope.config').values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+      require('telescope.pickers').new({}, {
+        prompt_title = 'Harpoon',
+        finder = require('telescope.finders').new_table { results = file_paths },
+        previewer = conf.file_previewer {},
+        sorter = conf.generic_sorter {},
+      }):find()
+    end
+
+    vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, { desc = 'Harpoon: add file' })
+    vim.keymap.set('n', '<C-e>', function() toggle_telescope(harpoon:list()) end, { desc = 'Harpoon: toggle menu' })
+
+    vim.keymap.set('n', '<M-h>', function() harpoon:list():select(1) end, { desc = 'Harpoon: file 1' })
+    vim.keymap.set('n', '<M-j>', function() harpoon:list():select(2) end, { desc = 'Harpoon: file 2' })
+    vim.keymap.set('n', '<M-k>', function() harpoon:list():select(3) end, { desc = 'Harpoon: file 3' })
+    vim.keymap.set('n', '<M-l>', function() harpoon:list():select(4) end, { desc = 'Harpoon: file 4' })
+  end,
+}
